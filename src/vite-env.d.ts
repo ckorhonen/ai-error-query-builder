beecf8a9d0ab04599721516c7bffadba97dff26d
Chris Korhonen <ckorhonen@gmail.com>
2025-10-16 07:09:05 -0400

    Add Vite environment variable type declarations

---
+++ src/vite-env.d.ts
@@ -0,0 +1,20 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_NAME?: string
+  readonly VITE_APP_VERSION?: string
+  readonly VITE_ENV?: string
+  readonly VITE_ENABLE_LLM_INTEGRATION?: string
+  readonly VITE_ENABLE_ANALYTICS?: string
+  readonly VITE_OPENAI_API_KEY?: string
+  readonly VITE_OPENAI_MODEL?: string
+  readonly VITE_ANTHROPIC_API_KEY?: string
+  readonly VITE_ANTHROPIC_MODEL?: string
+  readonly VITE_API_ENDPOINT?: string
+  readonly VITE_API_URL?: string
+  readonly VITE_API_BASE_URL?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
